3a2bfd74a11c17171764fe7044a8d4e9e3e1fa19
vmalkar <heresme.vivek@gmail.com>
2017-06-22 19:52:28 +0530

    Update pubsub.js

--- pubsub.js
+++ pubsub.js
@@ -1,6 +1,7 @@
 var redis = require("redis"),
     client = redis.createClient();
 
+client.publish("temp-reading:living-room", "37.0");
 client.subscribe("pubsub");
 client.on("message", function(channel, message){
   console.log(channel + ": " + message);
